514bae7f36c4272f0f389f1d0d480dcd28709422
“RWO-zb” <13762659881@163.com>
2025-04-10 20:17:04 +0800

    Dijkstra

--- graph/Dijkstra.cpp
+++ graph/Dijkstra.cpp
@@ -8,37 +8,34 @@ int a[maxx][maxx];
 void dijkstra(){
     memset(vis,false,sizeof(vis));
 	memset(dis,0x3f,sizeof(dis));
-    dis[1]=0;
+    dis[1]=0;//从第一个点开始
     int u=0;
     for(int i=1;i<=n;i++){
-        int mymin=maxx;
-        for(int j=1;j<=n;j++)
-            if(dis[j]<mymin&&!vis[j]){
-                mymin=dis[j];
-                u=j;
+        int mymin=0x3f;
+        for(int k=1;k<=n;k++){//寻找当前没有被访问的距离最小的点
+            if(!vis[k]&&dis[k]<mymin){
+                mymin=dis[k];
+                u=k;
             }
+        }
         vis[u]=true;
-        for(int j=1;j<=n;j++){
-            if(vis[j]==false&&dis[j]>dis[u]+a[u][j])
-                dis[j]=dis[u]+a[u][j];
+        for(int k=1;k<=n;k++){//更新当前的距离状态
+            if(!vis[k]&&dis[k]>dis[u]+a[u][k])
+                dis[k]=dis[u]+a[u][k];
         }
     }
 }
 int main() {
-    cout << "m,m ";
     cin >> n >> m;
-
-    // 初始化邻接矩阵
     memset(a, 0x3f, sizeof(a));
-    cout << "m" << m  << endl;
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++) {//输入m条边
         int u, v, w;
         cin >> u >> v >> w;
         a[u][v] = w;
         a[v][u] = w;
     }
     dijkstra();
-    for(int i=1;i<=n;i++)
+    for(int i=1;i<=n;i++)//输出结果
         cout<<dis[i]<<endl;
     return 0;
 }
